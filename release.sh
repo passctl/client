d9fe93581af1af881a7a4adf2027997a66d2e28d
ngn <ngn@ngn.tf>
2024-03-06 21:04:56 +0300

    remove debug info from releases

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 mkdir -p release
 
-CGO_ENABLED=0 go build
+CGO_ENABLED=0 go -ldflags "-s -w" build
 tar czvf release/passctl_linux-amd64.tar.gz client
 
-GOARCH=386 CGO_ENABLED=0 go build
+GOARCH=386 CGO_ENABLED=0 go -ldflags "-s -w" build
 tar czvf release/passctl_linux-386.tar.gz client
 
 rm -f client
